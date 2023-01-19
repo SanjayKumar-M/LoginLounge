c8fe300fe7a776781ef380ac7bb5138ab7076f0f
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-01-19 19:49:08 +0530

    Login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Register from './Components/Register';
 import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import Login from './Components/Login';
 function App() {
   return (
     <div className="App">
@@ -7,6 +8,7 @@ function App() {
       <Routes>
         <Route path='/register' element = {<Register />} />
         <Route path='/dashboard' element = {<h1> This is Dashboard</h1>} />
+        <Route path='/login' element = {<Login />} />
         <Route path='/' element = {<h1> This is Homepage</h1> } />
       </Routes>
       </BrowserRouter>
