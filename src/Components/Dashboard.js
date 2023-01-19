a438adeb521b01d9c718870b9652246daa3f677b
SanjayKumar-M <sanjaymoad786@gmail.com>
2023-01-20 01:26:37 +0530

    all fine

--- src/Components/Dashboard.js
+++ src/Components/Dashboard.js
@@ -3,34 +3,37 @@ import '../Styles/Dashboard.css'
 import Navigation from './Navigation'
 import { UserDetailsApi } from './Api'
 import LoadingOverlay from './Loading'
-import { Logout ,isAuthenticated} from './Authentication'
+import { Logout, isAuthenticated } from './Authentication'
 import { Navigate, useNavigate } from 'react-router-dom'
 
 const Dashboard = () => {
     const [Detail, setDetail] = useState({ name: "", email: "", localId: "" })
     const navigate = useNavigate()
-    useEffect(()=>{
-        UserDetailsApi().then((response)=>{
-            setDetail({
-                name: response.data.users[0].displayName,
-                email: response.data.users[0].email,
-                localId: response.data.users[0].localId
+    useEffect(() => {
+        if (isAuthenticated()) {
+            UserDetailsApi().then((response) => {
+                setDetail({
+                    name: response.data.users[0].displayName,
+                    email: response.data.users[0].email,
+                    localId: response.data.users[0].localId
+                })
             })
-        })
-    })
+        }
+
+    }, [])
 
     const logoutUser = () => {
         Logout()
         navigate('/login')
     }
-    if(!isAuthenticated()){
-        return <Navigate to='/login'/>
+    if (!isAuthenticated()) {
+        return <Navigate to='/login' />
 
     }
     return (
         <div>
-            <Navigation logoutUser = {logoutUser}/>
-            
+            <Navigation logoutUser={logoutUser} />
+
             {Detail.name && Detail.email && Detail.localId ?
                 (<>
                     <div className='dashcontainer'>
